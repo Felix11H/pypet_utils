80211cf390d3acc795a90578adef5ef944faf686
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-11-23 21:15:00 +0100

    load_trajectories() and get_names()

---
+++ pypet_utils.py
@@ -0,0 +1,40 @@
+
+from pypet import load_trajectory
+from pypet.brian2.parameter import Brian2Parameter, \
+                                   Brian2MonitorResult
+
+def load_trajectories(filename, brian2=False):
+
+    loading, idx = True, 0
+    trajectories = []
+
+    while loading:
+
+        if brian2:
+            tr = load_trajectory(index=idx, filename=filename,
+                                 dynamic_imports=[Brian2MonitorResult,
+                                             Brian2Parameter])
+        else:
+            tr = load_trajectory(index=idx, filename=filename)
+        trajectories.append(tr)
+        
+        idx += 1
+
+        try:
+            if brian2:
+                tr = load_trajectory(index=idx, filename=filename,
+                                     dynamic_imports=[Brian2MonitorResult,
+                                                 Brian2Parameter])
+            else:
+                tr = load_trajectory(index=idx, filename=filename)
+
+        except ValueError:
+            loading=False
+
+
+    return trajectories
+
+
+def contained_trajectories(filename, brian2=False):
+    trs = load_trajectories(filename, brian2)
+    return [tr.name for tr in trs]
